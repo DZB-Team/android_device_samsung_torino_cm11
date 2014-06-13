a6d05941165a5320dc9b1b7c7cb7efd5a8f7509b
raw99 <raw9965@gmail.com>
2014-06-14 00:13:25 +0300

    base

---
+++ README.md
@@ -0,0 +1,4 @@
+Device Galaxy Y Duos - CyanogenMod 11.0
+=====================================
+
+Device tree for Samsung Galaxy Y Duos S6102 - CM11.0
---
+++ cm.mk
@@ -0,0 +1,21 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit device configuration for totoro
+$(call inherit-product, device/samsung/torino/full_torino.mk)
+
+# Inherit some common CM stuff.
+TARGET_SCREEN_HEIGHT := 320
+TARGET_SCREEN_WIDTH := 240
+$(call inherit-product, vendor/cm/config/mini.mk)
+
+# Overrides
+PRODUCT_NAME := cm_torino
+PRODUCT_DEVICE := torino
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := GT-S6102
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_RELEASE_NAME := GalaxyTorino
+PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S6102
